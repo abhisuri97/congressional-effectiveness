4128db94b55a403012173d0fc832670023654691
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-07 15:06:00 -0400

    Add header section

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const index = require('../routes/index');
 // mongoose connection call
 mongoose.connect(config.database)
   .catch((err) => {
-    console.log('Error on mongodb connection: ')
+    console.log('Error on MongoDB connection: ')
     console.log(err.message);
     process.exit()
   })
@@ -23,13 +23,13 @@ app.set('secret', config.secret);
 // bodyparser middleware JIC
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '/../views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '../views/partials');
+hbs.registerPartials(__dirname + '/../views/partials');
 
 // routes
-app.use('/public',express.static(path.join(__dirname, '../public')));
-app.use('/', index);
+app.use('/public',express.static(path.join(__dirname, '../public')))
+app.use('/', index)
 
 app.get('/', function(req, res, next) {
   res.send('Hello World!')

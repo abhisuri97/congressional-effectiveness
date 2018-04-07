8e4fb082b8ee331981649d1a29252248d2f7cdd2
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-07 14:25:18 -0400

    Add Handlebars and routing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
+const hbs = require('hbs');
+
 const config = require('../config.js');
 const models = require('./models/allModels')
+const index = require('../routes/index');
 
 // mongoose connection call
 mongoose.connect(config.database)
@@ -17,6 +21,15 @@ const app = express()
 app.set('secret', config.secret);
 
 // bodyparser middleware JIC
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '../views/partials');
+
+// routes
+app.use('/public',express.static(path.join(__dirname, '../public')));
+app.use('/', index);
 
 app.get('/', function(req, res, next) {
   res.send('Hello World!')

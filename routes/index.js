dc3925eabf7279c3f59a4e5b3f93ec06a55a8461
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-07 16:55:38 -0400

    Add login/signup pages

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,12 @@ router.get('/committees', function(req, res, next) {
   })
 })
 
+router.get('/login', function(req, res, next) {
+  res.render('login')
+})
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup')
+})
+
 module.exports = router
